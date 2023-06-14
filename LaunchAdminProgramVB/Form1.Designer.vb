323566d6c17d08efb1592598d680824485d0759a
Pinou007 <lapipack@orange.fr>
2023-06-14 20:43:05 +0200

    Ajoutez des fichiers projet.

---
+++ LaunchAdminProgramVB/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(75, 19)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(269, 40)
+        Label1.TabIndex = 0
+        Label1.Text = "Your program is getting off the ground." & vbCrLf & "Thank you for your patience. "
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(12, 19)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(57, 40)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(375, 74)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label1)
+        FormBorderStyle = FormBorderStyle.None
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Opacity = 0.95R
+        RightToLeftLayout = True
+        ShowInTaskbar = False
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Launch in adminstrator mode"
+        TopMost = True
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ LaunchAdminProgramVB/Form1.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Diagnostics
+Imports System.IO
+Imports System.Threading
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim configFilePath As String = Path.Combine(Application.StartupPath, "Config.pinou007")
+        Dim processInfo As New ProcessStartInfo()
+
+        ' Vérifier si le fichier de configuration existe
+        If File.Exists(configFilePath) Then
+            ' Lire le nom du programme à lancer depuis le fichier de configuration
+            Dim programName As String = File.ReadAllText(configFilePath)
+
+            ' Configurer les informations du processus
+            processInfo.FileName = programName
+            processInfo.UseShellExecute = True
+            processInfo.Verb = "runas" ' Demande d'élévation des privilèges administratifs
+
+            ' Lancer le processus
+            Try
+                Thread.Sleep(1000)
+                Process.Start(processInfo)
+
+
+            Catch ex As Exception
+                ' Gérer les erreurs lors du lancement du processus
+                MsgBox("Une erreur s'est produite : " & ex.Message)
+
+            End Try
+        Else
+            ' Fichier de configuration introuvable
+            MsgBox("Le fichier de configuration est introuvable.")
+            End
+        End If
+        End
+
+    End Sub
+End Class
